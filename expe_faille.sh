49be4a6cf6c480e177019904abb68b5b7b0882e6
gandonni <nicolas.gandon78@grenoble-inp.org>
2020-12-07 20:45:43 +0100

    ajout de commentaires

--- expe_faille.sh
+++ expe_faille.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# On build puis lance le container sans la faille
 docker build -t faille_safety .
 docker run --rm faille_safety
 
+# On change le dockerfile
 mv Dockerfile Dockerfile-safe
 mv Dockerfile-unsafe Dockerfile
 
+# On build puis lance le container avec la faille
 docker build -t faille_safety .
 docker run --rm faille_safety
 
+# On remet le Dockerfile par defaut
 mv Dockerfile Dockerfile-unsafe
 mv Dockerfile-safe Dockerfile
